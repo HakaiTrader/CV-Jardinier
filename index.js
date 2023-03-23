7b64ea987223f1fd10a044de0b1cf7fbb984a708
HakaiTrader <musicus@live.be>
2023-03-23 10:27:31 +0100

    Mise à Jour.

--- index.js
+++ index.js
@@ -6,15 +6,51 @@ const scaling = () => {
     if (toggle === false) {
         attestation.style.transform = 'scale(20)';
         toggle = true;
-        if (window.innerWidth < 900 ) {
+        if (window.innerWidth < 900) {
             attestation.style.transform = 'scale(10)';
             toggle = true;
         }
 
     } else if (toggle = true) {
-        attestation.style.transform ='scale(1)'
+        attestation.style.transform = 'scale(1)'
         toggle = false;
     }
-    
+
 }
 attestation.addEventListener('click', scaling);
+
+// SLIDER //
+
+
+const slide = document.querySelectorAll('.slides')
+const next = document.querySelector('.next')
+const previous = document.querySelector('.prev')
+
+let etape = 0;
+let img = slide.length
+
+const removeActiveImg = () => {
+    for (let i = 0; i < img; i++) {
+        slide[i].classList.remove('active')
+
+    }
+}
+
+
+next.addEventListener('click', () => {
+    etape++;
+    if (etape >= img) {
+        etape = 0;
+    }
+    removeActiveImg()
+    slide[etape].classList.add('active');
+})
+previous.addEventListener('click', () => {
+    etape--;
+    if (etape < 0) {
+        etape = img - 1
+    }
+    removeActiveImg()
+    slide[etape].classList.add('active');
+})
+
